872c56c05f870ed3437fc0cbd308a82b878f08b8
Potsawat Thinkanwatthana <potsawattkw@outlook.com>
2022-07-25 05:58:56 +0700

    feat: Done 01418231-LAB-04 chef-yor

---
+++ Y2-01/01418231/LAB/02/chef-yor.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STR_LEN 32
+
+typedef enum { false, true } bool;
+
+typedef struct dish
+{
+    int listed;
+    char name[STR_LEN];
+    struct dish *next;
+}
+dish;
+
+static dish *menu_start = NULL;
+
+int main(void)
+{
+    dish *menu;
+    char s_buff[STR_LEN] = "";
+
+    dish *temp = menu_start;
+    int index = 0;
+    int id = 0;
+
+    while (1)
+    {
+        // printf("> ");
+        if (fgets(s_buff, STR_LEN, stdin) == NULL)
+        {
+            // exit(1);
+            exit(0);
+        }
+
+        switch (s_buff[0])
+        {
+            case 'a':
+                if (s_buff[1] == 'd'
+                    && s_buff[2] == 'd'
+                    && s_buff[3] == '\n'
+                    && s_buff[4] == '\0')
+                {
+                    // printf("+ ");
+                    fgets(s_buff, STR_LEN, stdin);
+
+                    dish *adish = malloc(sizeof *adish);
+
+                    if (menu_start == NULL)
+                    {
+                        menu_start = adish;
+                        adish->next = NULL;
+                    }
+                    else
+                    {
+                        temp = menu_start;
+
+                        while (temp->next != NULL)
+                            temp = temp->next;
+
+                        temp->next = adish;
+                    }
+
+                    for (int i = 0; s_buff[i] != '\0'; i++)
+                        if (s_buff[i] == '\n')
+                            s_buff[i] = '\0';
+
+                    strncpy(adish->name, s_buff, STR_LEN);
+                    adish->listed = true;
+                }
+
+                break;
+            case 'd':
+                if (s_buff[1] == 'e'
+                    && s_buff[2] == 'l'
+                    && s_buff[3] == '\n'
+                    && s_buff[4] == '\0')
+                {
+                    index = 0;
+                    id = 0;
+                    temp = menu_start;
+
+                    // printf("+ ");
+                    fgets(s_buff, STR_LEN, stdin);
+
+                    index = strtol(s_buff, NULL, 10);
+
+                    while (temp != NULL)
+                    {
+                        if ((id += temp->listed) == index)
+                            break;
+
+                        temp = temp->next;
+                    }
+
+                    if (temp != NULL && id == index)
+                        temp->listed = false;
+                }
+
+                break;
+            case 's':
+                if (s_buff[1] == 'h'
+                    && s_buff[2] == 'o'
+                    && s_buff[3] == 'w'
+                    && s_buff[4] == '\n'
+                    && s_buff[5] == '\0')
+                {
+                    index = 0;
+                    id = 0;
+                    temp = menu_start;
+
+                    printf("<< Menu >>\n");
+
+                    while (temp != NULL)
+                    {
+                        if (temp->listed == true)
+                            printf("%d. %s\n", id += temp->listed, temp->name);
+
+                        temp = temp->next;
+                    }
+                }
+
+                break;
+            case 'e':
+                if (s_buff[1] == 'x'
+                    && s_buff[2] == 'i'
+                    && s_buff[3] == 't'
+                    && s_buff[4] == '\n'
+                    && s_buff[5] == '\0')
+                {
+                    index = 0;
+                    id = 0;
+                    temp = menu_start;
+
+                    printf("<< Menu >>\n");
+
+                    while (temp != NULL)
+                    {
+                        if (temp->listed == true)
+                            printf("%d. %s\n", id += temp->listed, temp->name);
+
+                        temp = temp->next;
+                    }
+
+                    exit(0);
+                }
+
+                break;
+        }
+    }
+
+    return 0;
+}
